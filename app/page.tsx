2942fd9f3d19a43913560a877873d074192a3a9e
kingstech001 <mamahkingsleychukwuebuka@gmail.com>
2025-10-03 18:33:32 +0100

    feat: added the gallerycarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-// import TopBar from "@/app/components/TopBar";
-// import NavBar from "@/app/components/NavBar";
+
 import Hero from "@/app/components/Hero";
 import CategorySection from "./components/CategoriesSection";
 import NewArrivals from "./components/NewArrivals";
 import HeroBanner from "./components/HeroBanner";
+import GalleryCarousel from "./components/GalleryCarousel";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <CategorySection />
+      <GalleryCarousel />
       <NewArrivals />
       <HeroBanner />
       
